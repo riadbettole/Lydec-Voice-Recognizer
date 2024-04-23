4b81ad46583fb6301f2336b61b7ab9f29dda5d39
riadbettole <riadbettole@gmail.com>
2024-04-23 21:28:00 +0100

    testing ta7alil

--- example/urls.py
+++ example/urls.py
@@ -4,6 +4,7 @@ from django.urls import path
 from example.views import index
 from example.views import save_audio
 from example.views import admin
+from example.views import test
 from example.views import api_gpt4
 
 
@@ -11,5 +12,6 @@ urlpatterns = [
     path('save_transcription/', save_audio, name='save_audio1'),
     path('', index, name='save_audio2'),
     path('admin_lydec', admin, name='admin_lydec'),
+    path('test', test, name='test'),
     path('api_gpt4', api_gpt4, name='api_gpt4'),
 ]
